87a1a17009c7586f7a6d4ae5af673ad99e1fdf44
Emod Kovacs <kovacsemod@gmail.com>
2025-12-08 00:53:30 +0000

    fix: disable trailing slash, hoping webhook will no longer redirect?

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   serverExternalPackages: ['canvas', '@napi-rs/canvas', 'pdfjs-dist'],
+  trailingSlash: false,
 };
 
 export default nextConfig;
